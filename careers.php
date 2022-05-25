4f41b769bef7c4b4b8cb6e614c32f53070c22ee4
Dillon Ellis <dellis@fcul.com>
2022-05-25 15:08:34 -0400

    original careers page

---
+++ careers.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<html lang="en">
+<head>
+<title>LSCU | Career Opportunities</title>
+<meta charset="utf-8">
+<!-- Meta -->
+<meta name="keywords" content="" />
+<meta name="author" content="">
+<meta name="robots" content="" />
+<meta name="description" content="" />
+
+<!-- this styles only adds some repairs on idevices  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="../images/favicon.ico">
+
+<!-- Google fonts - witch you want to use - (rest you can just remove) -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- stylesheets -->
+<link rel="stylesheet" media="screen" href="../js/bootstrap/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="../js/megamenu/stylesheets/screen.css" type="text/css" />
+<link rel="stylesheet" href="../css/default.css" type="text/css" />
+<link rel="stylesheet" href="../css/layouts.css" type="text/css" />
+<link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
+<link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
+<link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
+<link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
+<link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-12265204-1');
+</script>
+
+</head>
+
+<body>
+<div class="site_wrapper">
+  <div class="topbar light topbar-padding">
+    <?php include '../inc/topbar.php';?>
+  </div>
+  <div class="clearfix"></div>
+  
+  <div id="header" class="header-section style1 pin-style">
+    <?php include '../inc/nav.php';?>
+  </div>
+  <!--end menu-->
+  <div class="clearfix"></div>
+  
+  <section>
+    <div class="header-inner hr">
+      <div class="inner text-center">
+        <h4 class="titleHR uppercase">Careers</h4>
+      </div>
+      <img src="imgs/inner-headerBG.jpg" alt="" class="img-responsive"/></div>
+  </section>
+  <!-- end header inner -->
+  <div class="clearfix"></div>
+	
+	<section class="sec-padding" style="padding-top: 30px; padding-bottom: 20px;">
+    <div class="container">	
+	<div class="row sec-padding">
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small right"><span class="icon-plane"></span></div>
+            <div class="text-box-left">
+              <h4 class="less-mar3">Our History</h4>
+              <p>Share the highlights of our history, from its founding to present day. Candidates want to learn how we have evolved and how they can help it grow.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item-->
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small left"><span class="icon-screen-desktop"></span></div>
+            <div class="text-box-right">
+              <h4 class="less-mar3">Our Mission</h4>
+              <p>It’s the bottom line of our company’s purpose. People want to work for companies that inspire them, so add a statement of our vision: what we want to achieve and how we work toward our goals.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item-->
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small right"><span class="icon-docs"></span></div>
+            <div class="text-box-left">
+              <h4 class="less-mar3">Our Values</h4>
+              <p>Our values define your company’s personality. It’s what makes our company unique. They clarify what qualities our prioritizes.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item-->
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small left"><span class="icon-handbag"></span></div>
+            <div class="text-box-right">
+              <h4 class="less-mar3">Our People</h4>
+              <p>We thrive because of our people. Spotlight employees and their stories and introduce our teams and managers to humanize our page.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item-->
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small right"><span class="icon-camcorder"></span></div>
+            <div class="text-box-left">
+              <h4 class="less-mar3">Our Day-to-Day Life</h4>
+              <p>Candidates want to see how working at our company is like. It helps them picture themselves as members of your team.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item-->
+        
+        <div class="col-md-6 col-sm-6">
+          <div class="feature-box8">
+            <div class="iconbox-small left"><span class="icon-bubble"></span></div>
+            <div class="text-box-right">
+              <h4 class="less-mar3">Our Workspace</h4>
+              <p>Candidates want to know more about their future workspaces. Let them know the location of your offices, how they are designed (e.g. open-plan, meeting rooms) and what kind of technology they’ll get access to.</p>
+            </div>
+          </div>
+        </div>
+        <!--end item--> 
+        
+      </div>
+		</div>
+	</section>
+  
+ <section class="sec-padding" style="padding-top: 30px; padding-bottom: 20px;">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-md-12 col-sm-12 col-xs-12 bmargin">
+			<p>The credit union movement began with a simple idea – that people could achieve a better standard of living for themselves and others by pooling their savings and making loans to neighbors and co-workers. The credit union philosophy is “Not for Profit, Not for Charity, But for Service.”</p>
+			
+			<div class="accordion_holder">
+          <div class="accordion_example1"> 
+            
+            <!-- Section 1 -->
+            <div class="accordion_in two acc_active">
+              <div class="acc_head">What is a credit union?</div>
+              <div class="acc_content">
+				  <p>Credit unions offer all the same products as banks, but we don’t have any outside shareholders. Instead, our members are our shareholders. That means our profits are reinvested in you allowing you to enjoy better rates and better service. Credit unions offer special advantages:</p>
+				  
+				  <div class="list-style-2">
+          			<div class="icon"><i class="fa fa-angle-right"></i></div>
+          			<div class="text"><strong>Member ownership</strong> – Each member is a part owner of the credit union to which they belong and is entitled to one vote, regardless of how much money he or she has deposited. </div>
+        		</div>
+        		<!--end item-->
+				  
+				  <div class="list-style-2">
+          			<div class="icon"><i class="fa fa-angle-right"></i></div>
+          			<div class="text"><strong>Members come first</strong> – Because members are our shareholders, our profits come back to members in the form of better rates on all accounts. Members also enjoy lower fees than they would at for-profit institutions.</div>
+        		</div>
+        		<!--end item-->
+				  
+				  <div class="list-style-2">
+          			<div class="icon"><i class="fa fa-angle-right"></i></div>
+          			<div class="text"><strong>Special services</strong> – We serve our members and offer special services that for-profit institutions may not always be able to match. Examples include small denomination signature loans to help members through financial difficulties, second-chance or credit rebuilding loans, savings clubs for kids and seniors and financial counseling.</div>
+        		</div>
+        		<!--end item-->
+				  
+				  <div class="list-style-2">
+          			<div class="icon"><i class="fa fa-angle-right"></i></div>
+          			<div class="text"><strong>Volunteer leadership</strong> – Credit unions are led by a board of directors elected from our membership. Board members volunteer their time and service and are not compensated, which means decisions are made based on the best interest of the members. Any member can be nominated to serve on the board of their credit union.</div>
+        		</div>
+        		<!--end item-->
+				  
+				  <p>Credit unions are safe and sound places to bank. Every credit union is regulated and examined consistently by the National Credit Union Administration (NCUA) - the U.S. government agency - or a state credit union government agency, ensuring the safety and soundness of its operations. And, just like the FDIC, the NCUA insures credit union deposits up to at least $250,000.</p>
+				  <p>We are thriving today, with more than 124 million members across the country. For more information about credit unions, and to find the one that’s right for you, visit <a href="https://yourmoneyfurther.com" target="_blank">America's Credit Unions</a>.</p>
+
+			  </div>
+            </div>
+            
+            <!-- Section 2 -->
+            <div class="accordion_in two">
+              <div class="acc_head">Who can join a credit union?</div>
+              <div class="acc_content">
+				  
+				  <p>It’s easy to join the 124 million Americans already enjoying the benefits of membership. Credit unions are open to a wide range of people. Whether membership is community based, affiliated with an employee group, church or school, more people than you think can join and enjoy the benefits of a credit union. Plus, many credit unions are open to immediate family of qualifying members. <a href="https://yourmoneyfurther.com" target="_blank">Find the right credit union for you</a>.</p>
+				  
+			  </div>
+            </div>
+			  
+			  <!-- Section 3 -->
+            <div class="accordion_in two">
+              <div class="acc_head">How are credit unions different?</div>
+              <div class="acc_content">
+				  
+				  <p>While we offer all the same products as banks, there is one major difference between credit unions and other financial institutions – our members are our owners. This means profits are reinvested in the member in the form of better rates on all accounts.</p>
+				  
+			  </div>
+            </div>
+			  
+			  <!-- Section 4 -->
+            <div class="accordion_in two">
+              <div class="acc_head">What services do credit unions offer?</div>
+              <div class="acc_content">
+				  
+				  <p>Credit unions offer a wide array of services, each designed to meet the needs of our members. Each credit union is a little different, based on the specific needs of its members. Some may offer deposits and loans, while others offer all the same services as other financial institutions. </p>
+				  
+				  <p>&nbsp;</p>
+				  
+				  <center>
+			<div class="box" style="width: 50%;">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-link"></i></div>
+					<div class="info">
+						<h3 class="title" style="margin-top: 25px;">LSCU Profile</h3>
+
+						<div class="more">
+							<a href="profile.php">
+								View LSCU <i class="fa fa-angle-double-right"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div>
+			</center>
+				  
+				  <p>&nbsp;</p>
+				  
+				  <center>
+			<div class="box" style="width: 50%;">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-link"></i></div>
+					<div class="info">
+						<h3 class="title" style="margin-top: 25px;">Helping People Afford Life</h3>
+
+						<div class="more">
+							<a href="../comms/hpal.php" aria-label="View Now">
+								View Now <i class="fa fa-angle-double-right"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div>
+			</center>
+				  
+			  </div>
+            </div>
+
+          </div>
+        </div>
+        <!-- Accordion end -->
+
+			
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  <!-- end section -->
+  <div class="clearfix"></div>
+  
+  <section class="section-copyrights sec-moreless-padding">
+	<?php include '../inc/footer.php';?>
+  </section>
+  <!--end section-->
+  <div class="clearfix"></div>
+  
+  <a href="#" class="scrollup blue"><span style="display:none;">Scroll To Top Button</span></a><!-- end scroll to top of the page--> 
+  
+</div>
+<!--end sitewraper--> 
+
+<!-- ========== Js files ========== --> 
+
+<script type="text/javascript" src="../js/universal/jquery.js"></script> 
+<script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script> 
+<script src="../js/megamenu/js/main.js"></script> 
+<script src="../js/mainmenu/jquery.sticky.js"></script> 
+<script src="../js/scrolltotop/totop.js"></script> 
+<script type="text/javascript" src="../js/tabs/smk-accordion.js"></script>
+<script type="text/javascript" src="../js/tabs/custom.js"></script>
+<script src="../js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+<script src="../js/scripts/functions.js" type="text/javascript"></script>
+</body>
+</html>
