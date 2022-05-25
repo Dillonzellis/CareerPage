40c53ff85590017afc3c4d28625b80802b1ee03c
Dillon Ellis <dellis@fcul.com>
2022-05-25 19:04:23 -0400

    hero img

---
+++ careerDE.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<html lang="en">
+
+<head>
+  <title>LSCU | Career Opportunities</title>
+  <meta charset="utf-8">
+  <!-- Meta -->
+  <meta name="keywords" content="" />
+  <meta name="author" content="">
+  <meta name="robots" content="" />
+  <meta name="description" content="" />
+
+  <!-- this styles only adds some repairs on idevices  -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="../images/favicon.ico">
+
+  <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+  <!--[if lt IE 9]>
+<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+  <!-- stylesheets -->
+  <link rel="stylesheet" media="screen" href="../js/bootstrap/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="../js/megamenu/stylesheets/screen.css" type="text/css" />
+  <link rel="stylesheet" href="../css/default.css" type="text/css" />
+  <link rel="stylesheet" href="../css/layouts.css" type="text/css" />
+  <link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
+  <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
+  <link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
+  <link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
+  <link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
+  <link rel="stylesheet" href="style.css">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-12265204-1');
+  </script>
+
+</head>
+
+<body>
+  <div class="site_wrapper">
+    <div class="topbar light topbar-padding">
+      <?php include '../inc/topbar.php'; ?>
+    </div>
+    <div class="clearfix"></div>
+
+    <div id="header" class="header-section style1 pin-style">
+      <?php include '../inc/nav.php'; ?>
+    </div>
+    <!--end menu-->
+    <div class="clearfix"></div>
+
+    <section>
+      <div class="hero-wrapper">
+        <div class="hero-text-wrapper">
+          <h1 class="hero-text">CAREERS</h1>
+          <hr class="hero-hr" />
+        </div>
+        <div class="hero-btn-wrapper">
+          <button class="hero-btn btn">SEARCH CAREERS</button>
+        </div>
+      </div>
+    </section>
+
+
+  </div>
+  <div class="clearfix"></div>
+
+  <section class="section-copyrights sec-moreless-padding">
+    <?php include '../inc/footer.php'; ?>
+  </section>
+  <!--end section-->
+  <div class="clearfix"></div>
+
+  <a href="#" class="scrollup blue"><span style="display:none;">Scroll To Top Button</span></a><!-- end scroll to top of the page-->
+
+  </div>
+  <!--end sitewraper-->
+
+  <!-- ========== Js files ========== -->
+
+  <script type="text/javascript" src="../js/universal/jquery.js"></script>
+  <script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+  <script src="../js/megamenu/js/main.js"></script>
+  <script src="../js/mainmenu/jquery.sticky.js"></script>
+  <script src="../js/scrolltotop/totop.js"></script>
+  <script type="text/javascript" src="../js/tabs/smk-accordion.js"></script>
+  <script type="text/javascript" src="../js/tabs/custom.js"></script>
+  <script src="../js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+  <script src="../js/scripts/functions.js" type="text/javascript"></script>
+</body>
+
+</html>
