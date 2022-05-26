4dc3aab8e38d1e8945a7b5a45f3774fdb1323fe1
Dillon Ellis <dellis@fcul.com>
2022-05-26 10:19:40 -0400

    values basic formatting

--- careerDE.php
+++ careerDE.php
@@ -22,11 +22,8 @@
     <link rel="shortcut icon" href="../images/favicon.ico">
 
     <!-- Google fonts - witch you want to use - (rest you can just remove) -->
-    <link
-        href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic'
-        rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet'
-        type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 
     <!--[if lt IE 9]>
 <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -40,24 +37,34 @@
     <link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
     <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css"
-        media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
     <link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
     <link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
     <link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
     <link rel="stylesheet" href="style.css">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Raleway:wght@400;600;700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="./css/de-normalize.css" />
+    <link rel="stylesheet" href="./css/de-style.css" />
+
+
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-    gtag('config', 'UA-12265204-1');
+        gtag('config', 'UA-12265204-1');
     </script>
 
 </head>
@@ -75,6 +82,7 @@
         <!--end menu-->
         <div class="clearfix"></div>
 
+        <!-- hero -->
         <section>
             <div class="hero-wrapper">
                 <div class="hero-text-wrapper">
@@ -83,6 +91,108 @@
                 </div>
                 <div class="hero-btn-wrapper">
                     <button class="hero-btn btn">SEARCH CAREERS</button>
+                </div>
+            </div>
+        </section>
+
+        <!-- values -->
+        <section>
+            <div class="container values-container">
+                <!-- <div class="container-fluid values-container"> -->
+                <div class="row values-row">
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="value-text-container right-text">
+                                    <h2 class="value-header">Our History</h2>
+                                    <hr class="value-hr" />
+                                    <p class="value-text">
+                                        Share the highlights of our history, from its founding to
+                                        present day. Candidates want to learn how we have evolved
+                                        and how they can help it grow.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="value-item-wrapper">
+                                    <div class="svg-container">
+                                        <!-- <svg class="value-svg">value svg</svg> -->
+                                        <div>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat, voluptates dolore ex saepe distinctio,
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="value-item-wrapper">
+                            <div class="value-text-container">
+                                <h2 class="value-header">Our History</h2>
+                                <p class="value-text">
+                                    Share the highlights of our history, from its founding to
+                                    present day. Candidates want to learn how we have evolved
+                                    and how they can help it grow.
+                                </p>
+                            </div>
+                            <div class="svg-container">
+                                <svg class="value-svg">value svg</svg>
+                            </div>
+                        </div>
+                        <div class="value-item-wrapper">
+                            <div class="value-text-container">
+                                <h2 class="value-header">Our History</h2>
+                                <p class="value-text">
+                                    Share the highlights of our history, from its founding to
+                                    present day. Candidates want to learn how we have evolved
+                                    and how they can help it grow.
+                                </p>
+                            </div>
+                            <div class="svg-container">
+                                <svg class="value-svg">value svg</svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="value-item-wrapper">
+                            <div class="value-text-container">
+                                <h2 class="value-header">Our History</h2>
+                                <p class="value-text">
+                                    Share the highlights of our history, from its founding to
+                                    present day. Candidates want to learn how we have evolved
+                                    and how they can help it grow.
+                                </p>
+                            </div>
+                            <div class="svg-container">
+                                <svg class="value-svg">value svg</svg>
+                            </div>
+                        </div>
+                        <div class="value-item-wrapper">
+                            <div class="value-text-container">
+                                <h2 class="value-header">Our History</h2>
+                                <p class="value-text">
+                                    Share the highlights of our history, from its founding to
+                                    present day. Candidates want to learn how we have evolved
+                                    and how they can help it grow.
+                                </p>
+                            </div>
+                            <div class="svg-container">
+                                <svg class="value-svg">value svg</svg>
+                            </div>
+                        </div>
+                        <div class="value-item-wrapper">
+                            <div class="value-text-container">
+                                <h2 class="value-header">Our History</h2>
+                                <p class="value-text">
+                                    Share the highlights of our history, from its founding to
+                                    present day. Candidates want to learn how we have evolved
+                                    and how they can help it grow.
+                                </p>
+                            </div>
+                            <div class="svg-container">
+                                <svg class="value-svg">value svg</svg>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
