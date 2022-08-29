4e6ec360ac508cb98d9849f3d6b505780768a724
Dillon Ellis <dellis@fcul.com>
2022-08-29 16:08:35 -0400

    hide testimonial animation on mobile

--- careers.php
+++ careers.php
@@ -22,11 +22,8 @@
     <link rel="shortcut icon" href="../images/favicon.ico">
 
     <!-- Google fonts - witch you want to use - (rest you can just remove) -->
-    <link
-        href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic'
-        rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet'
-        type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 
     <!--[if lt IE 9]>
 <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -42,16 +39,13 @@
     <link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
     <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css"
-        media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
     <link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
     <link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
     <link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Montserrat:wght@400;600;700;800&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="../css/style-de.css" />
 
@@ -59,14 +53,14 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-    gtag('config', 'UA-12265204-1');
+        gtag('config', 'UA-12265204-1');
     </script>
 
 </head>
@@ -124,38 +118,30 @@
 
                                 <ul class="social-links" aria-label="social links">
                                     <li>
-                                        <a href="https://www.linkedin.com/company/league-of-southeastern-credit-unions"
-                                            aria-label="Linkedin" target="_blank">
+                                        <a href="https://www.linkedin.com/company/league-of-southeastern-credit-unions" aria-label="Linkedin" target="_blank">
                                             <div class="circle">
-                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Linkedin In.svg"
-                                                    alt="" srcset="" style="top: 48%" />
+                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Linkedin In.svg" alt="" srcset="" style="top: 48%" />
                                             </div>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.facebook.com/LeagueofSoutheasternCreditUnions/"
-                                            aria-label="Facebook" target="_blank">
+                                        <a href="https://www.facebook.com/LeagueofSoutheasternCreditUnions/" aria-label="Facebook" target="_blank">
                                             <div class="circle">
-                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Facebook F.svg"
-                                                    alt="" srcset="" style="left: 48%" />
+                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Facebook F.svg" alt="" srcset="" style="left: 48%" />
                                             </div>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.youtube.com/c/LeagueofSECUs" aria-label="Youtube"
-                                            target="_blank">
+                                        <a href="https://www.youtube.com/c/LeagueofSECUs" aria-label="Youtube" target="_blank">
                                             <div class="circle">
-                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Youtube.svg"
-                                                    alt="" srcset="" />
+                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Youtube.svg" alt="" srcset="" />
                                             </div>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://twitter.com/LeagueofSECUs" aria-label="Twitter"
-                                            target="_blank">
+                                        <a href="https://twitter.com/LeagueofSECUs" aria-label="Twitter" target="_blank">
                                             <div class="circle">
-                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Twitter.svg"
-                                                    alt="" srcset="" />
+                                                <img class="img-de" src="imgs/Career-Page/imgs/socials/Twitter.svg" alt="" srcset="" />
                                             </div>
                                         </a>
                                     </li>
@@ -184,8 +170,7 @@
                     <div class="accord-col">
                         <details>
                             <summary>Competitive Benefits</summary>
-                            <img src="imgs/Career-Page/imgs/benefits/benefits-img.png" class="img-de" alt=""
-                                srcset="" />
+                            <img src="imgs/Career-Page/imgs/benefits/benefits-img.png" class="img-de" alt="" srcset="" />
                             <p>
                                 We believe in supporting our team and their families by offering
                                 competitive and comprehensive benefits plans. We offer a variety
@@ -197,8 +182,7 @@
                         </details>
                         <details>
                             <summary>Professional Development</summary>
-                            <img src="imgs/Career-Page/imgs/benefits/development-img.png" class="img-de" lt=""
-                                srcset="" />
+                            <img src="imgs/Career-Page/imgs/benefits/development-img.png" class="img-de" lt="" srcset="" />
                             <p>
                                 Gain the confidence to advance your career with our professional
                                 development, tuition reimbursements, and our Emerging Leaders
@@ -210,8 +194,7 @@
                         </details>
                         <details>
                             <summary>Work-Life Balance</summary>
-                            <img src="imgs/Career-Page/imgs/benefits/work-life-img.png" alt="" class="img-de" srcset=""
-                                style="border-radius: 8px" />
+                            <img src="imgs/Career-Page/imgs/benefits/work-life-img.png" alt="" class="img-de" srcset="" style="border-radius: 8px" />
                             <p>
                                 Take control of your work-life synergy with flexible schedules
                                 and a supportive work environment. Get more satisfaction from
@@ -239,8 +222,7 @@
                         </details>
                         <details>
                             <summary>Team Engagement</summary>
-                            <img src="imgs/Career-Page/imgs/benefits/engagement-img.png" class="img-de" alt=""
-                                srcset="" />
+                            <img src="imgs/Career-Page/imgs/benefits/engagement-img.png" class="img-de" alt="" srcset="" />
                             <p>
                                 Our engagement committee provides the opportunity for teams
                                 across our footprint to get to know one another and nurture
@@ -252,8 +234,7 @@
                         </details>
                         <details>
                             <summary>Our Values</summary>
-                            <img class="values-benefits img-de" src="imgs/Career-Page/imgs/benefits/values-img.png"
-                                alt="" srcset="" />
+                            <img class="values-benefits img-de" src="imgs/Career-Page/imgs/benefits/values-img.png" alt="" srcset="" />
                             <p>
                                 Our culture is driven by our values. Our team members, whether
                                 out in the field building relationships or in the office
@@ -274,8 +255,7 @@
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Lina-testimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Lina-testimonial.png" alt="" srcset="" />
                                     <p class="card-title">Lina Guzman</p>
                                     <p class="card-sub-title">Administrative Assistant</p>
                                 </div>
@@ -290,11 +270,19 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "What I love most about working for LSCU & Affiliates is the
+                                people. They care about you as a person and as a colleague.
+                                You are treated with respect and professionalism every day.
+                                They foster a family atmosphere while providing you the
+                                opportunity to grow."
+                            </p>
+                        </div>
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Mike-testimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Mike-testimonial.png" alt="" srcset="" />
                                     <p class="card-title">Mike Miller</p>
                                     <p class="card-sub-title">Communications Manager</p>
                                 </div>
@@ -310,11 +298,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "LSCU & Affiliates is a great place to work! The people and
+                                the culture here are unlike anything I’ve seen before.
+                                Everyone works together. It’s a team effort from department
+                                to department. And the company has great pay, benefits, and
+                                social events to keep you feeling like you matter. Highly
+                                recommend!"
+                            </p>
+                        </div>
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Marcus-testimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Marcus-testimonial.png" alt="" srcset="" />
                                     <p class="card-title">Marcus King</p>
                                     <p class="card-sub-title">VP, Audit and Compliance</p>
                                 </div>
@@ -331,11 +328,21 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "LSCU & Affiliates is the only company I have ever worked
+                                for. I started right out of college and have been promoted
+                                throughout my career to now the VP of Audit and Compliance.
+                                I am trusted to do my job and I know when I do a great job,
+                                I will be rewarded. LSCU & Affiliates invests in its
+                                employees and gives them the best chance to succeed through
+                                tuition reimbursement and professional development."
+                            </p>
+                        </div>
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Kristi-testimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Kristi-testimonial.png" alt="" srcset="" />
                                     <p class="card-title">Kristi Arrington</p>
                                     <p class="card-sub-title">VP, Growth by Design</p>
                                 </div>
@@ -351,11 +358,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "At LSCU & Affiliates, we get to live out the credit union
+                                philosophy of people helping people. It makes a world of
+                                difference to believe in the work you are doing and the
+                                people you are doing the work with. When colleagues and
+                                clients start feeling more like extended family, you know
+                                you are in the right place."
+                            </p>
+                        </div>
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Melissa-tesimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Melissa-tesimonial.png" alt="" srcset="" />
                                     <p class="card-title">Melissa Brown</p>
                                     <p class="card-sub-title">Senior Accountant</p>
                                 </div>
@@ -371,11 +387,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "I love working for LSCU & Affiliates because of the
+                                intelligent, passionate people I get to work with. I also
+                                enjoy working in a company that is growing and always
+                                provides more career opportunities. LSCU offers great
+                                benefits that help the employee to feel rewarded for the
+                                good job that they do."
+                            </p>
+                        </div>
                         <div class="flip-card">
                             <div class="flip-card-inner">
                                 <div class="flip-card-front">
-                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Gerald-testimonial.png"
-                                        alt="" srcset="" />
+                                    <img class="img-de" src="imgs/Career-Page/imgs/testimonials/Gerald-testimonial.png" alt="" srcset="" />
                                     <p class="card-title">Gerald McSwain Jr.</p>
                                     <p class="card-sub-title">
                                         Facilities and Operations Manager
@@ -394,6 +419,17 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="flip-card-back-mobile">
+                            <p>
+                                "I love working for LSCU & Affiliates because I feel like I
+                                make a difference. I believe in our core values and what we
+                                represent as an organization. Understanding my role as the
+                                Facilities Manager is significant because of my
+                                responsibility to ensure a safe, secure, and productive
+                                environment for our team members, so that they can work hard
+                                to serve our members."
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -402,7 +438,7 @@
 
 
 <section class="section-copyrights sec-moreless-padding">
-    <?php include '../inc/footer.php';?>
+    <?php include '../inc/footer.php'; ?>
 </section>
 <!--end section-->
 <div class="clearfix"></div>
